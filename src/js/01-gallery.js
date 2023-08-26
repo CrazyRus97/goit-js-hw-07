3311f529ee165330041d051def913a5cecdc8e35
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-08-26 13:52:18 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,9 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery")
+
+const renderlist = (arr) => arr.map(item => (`<li class="gallery__item js-product-item"><img class="gallery__image" src=${item.preview} alt=${item.description}/></li>`)).join("");
+
+listEl.insertAdjacentHTML("beforeend", renderlist(galleryItems))
