4c17dd06c8feaccdec16cc975c4b6fa51a08af1c
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-08-26 15:33:13 +0300

    finall

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,6 +5,26 @@ console.log(galleryItems);
 
 const listEl = document.querySelector(".gallery")
 
-const renderlist = (arr) => arr.map(item => (`<li class="gallery__item js-product-item"><img class="gallery__image" src=${item.preview} alt=${item.description}/></li>`)).join("");
+const renderlist = (arr) => arr.map(item => (`<li data-id=${arr.indexOf(item)} class="gallery__item gallery__link js-product-item"><img class="gallery__image" src=${item.preview} alt=${item.description}/></li>`)).join("");
+
+const handleListClick = (event) => {
+    if (event.currentTarget === event.target) {
+        return;
+    }
+    const currentListItem = event.target.closest(".js-product-item");
+    const itemId = +currentListItem.dataset.id;
+    console.log(itemId)
+    // const galleryItem = galleryItems.find(item => {
+    //     galleryItems.indexOf(item) === itemId
+    // })
+    const galleryItem = galleryItems[itemId]
+
+    console.log(galleryItem)
+
+    const modalInstance = basicLightbox.create(`<div class="gallery__item"><img class="gallery__image" src=${galleryItem.original} alt=${galleryItem.description}/></div>`);
+    
+    modalInstance.show();
+}
 
 listEl.insertAdjacentHTML("beforeend", renderlist(galleryItems))
+listEl.addEventListener("click", handleListClick)
